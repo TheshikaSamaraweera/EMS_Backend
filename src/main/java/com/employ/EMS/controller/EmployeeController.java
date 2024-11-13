f5947cb3d8947fa2350ac1cb4a31f2d0842ef3e4
Theshika Navod <theshikanavod@gmail.com>
2024-11-13 20:39:50 +0530

    EMS 002 : Complete CRUD Operation

--- src/main/java/com/employ/EMS/controller/EmployeeController.java
+++ src/main/java/com/employ/EMS/controller/EmployeeController.java
@@ -2,12 +2,15 @@ package com.employ.EMS.controller;
 
 
 import com.employ.EMS.dto.EmployeeDto;
+import com.employ.EMS.entity.Employee;
 import com.employ.EMS.service.EmployeeService;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/employees")
@@ -23,6 +26,32 @@ public class EmployeeController {
     public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto) {
         EmployeeDto savedEmployee = employeeService.createEmployee(employeeDto);
         return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable("id") Long id) {
+        EmployeeDto employeeDto = employeeService.getEmployeeById(id);
+        return ResponseEntity.ok(employeeDto);
+
+    }
+
+    @GetMapping
+    public ResponseEntity<List<EmployeeDto>> getAllEmployees() {
+        List<EmployeeDto> employeeDtoList = employeeService.getAllEmployees();
+        return ResponseEntity.ok(employeeDtoList);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<EmployeeDto> updateEmployee(@PathVariable("id") Long id,
+                                                      @RequestBody EmployeeDto employeeDto) {
+        EmployeeDto updatedEmployee = employeeService.updateEmployee(id ,employeeDto);
+        return ResponseEntity.ok(updatedEmployee);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable("id") Long id) {
+        employeeService.deleteEmployee(id);
+        return ResponseEntity.ok("Employee deleted successfully!  ID: " + id);
     }
 
 
--- src/main/java/com/employ/EMS/service/EmployeeService.java
+++ src/main/java/com/employ/EMS/service/EmployeeService.java
@@ -3,10 +3,14 @@ package com.employ.EMS.service;
 
 import com.employ.EMS.dto.EmployeeDto;
 import com.employ.EMS.entity.Employee;
+import com.employ.EMS.exceptions.ResourceNotFoundException;
 import com.employ.EMS.mappre.EmployeeMapper;
 import com.employ.EMS.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 
@@ -21,7 +25,39 @@ public class EmployeeService {
         Employee savedEmployee = employeeRepository.save(employee);
 
         return EmployeeMapper.entityToDto(savedEmployee);
+    }
+
+    public EmployeeDto getEmployeeById(Long id) {
+       Employee employee= employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not found with id: " + id));
+
+        return EmployeeMapper.entityToDto(employee);
+    }
 
 
+    public List<EmployeeDto> getAllEmployees() {
+        List<Employee> list = employeeRepository.findAll();
+        return list.stream().map((Elist) -> EmployeeMapper.entityToDto(Elist))
+                .collect(Collectors.toList());
+    }
+
+    public EmployeeDto updateEmployee(Long id, EmployeeDto employeeDto) {
+        Employee employee = employeeRepository.findById(id)
+               .orElseThrow(() -> new ResourceNotFoundException("Employee not found with id: " + id));
+
+        employee.setFirstName(employeeDto.getFirstName());
+        employee.setLastName(employeeDto.getLastName());
+        employee.setEmail(employeeDto.getEmail());
+
+        Employee updatedEmployee = employeeRepository.save(employee);
+        return EmployeeMapper.entityToDto(updatedEmployee);
+    }
+
+
+    public void deleteEmployee(Long id) {
+        Employee employee = employeeRepository.findById(id)
+               .orElseThrow(() -> new ResourceNotFoundException("Employee not found with id: " + id));
+
+        employeeRepository.delete(employee);
     }
 }
